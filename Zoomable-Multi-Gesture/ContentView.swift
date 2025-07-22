6b62a9e960817fa246f701ac59e326f36675a7af
pakuVision <boardguy.vision@gmail.com>
2025-07-22 18:17:35 +0900

    Initial Commit

---
+++ Zoomable-Multi-Gesture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zoomable-Multi-Gesture
+//
+//  Created by boardguy.vision on 2025/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zoomable-Multi-Gesture/Zoomable_Multi_GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zoomable_Multi_GestureApp.swift
+//  Zoomable-Multi-Gesture
+//
+//  Created by boardguy.vision on 2025/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Zoomable_Multi_GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
